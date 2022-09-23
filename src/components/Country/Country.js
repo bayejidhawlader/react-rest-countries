2f7a4ecdff1ae732f79fd0b2ba9706dcfaaccfc1
Bayejid Hawlader <bayejidhawlader@gmail.com>
2022-09-23 21:35:32 +0600

    trying to add build and node_modules

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import './Country.css'
+import React from "react";
+import "./Country.css";
 
 const Country = (props) => {
-    const {area, region, name, flags} = props.country;
-    console.log(props.country);
-    return (
-        <div className='country'>
-            <h2>Name: {name.common}</h2>
-            <img src={flags.png} alt=""/>
-            <p>Area: {area}</p>
-            <p><small>Region: {region}</small></p>
-        </div>
-    );
+  const { area, region, name, flags } = props.country;
+  console.log(props.country);
+  return (
+    <div className="country">
+      <h2>Name:: {name.common}</h2>
+      <img src={flags.png} alt="" />
+      <p>Area: {area}</p>
+      <p>
+        <small>Region: {region}</small>
+      </p>
+    </div>
+  );
 };
 
 export default Country;
